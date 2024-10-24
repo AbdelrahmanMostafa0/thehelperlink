00500b1413f4a9a503d5625cb124717340ea3ed9
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2024-10-24 14:11:31 +0300

    feat:add dashboard section

--- src/Charts/BubbleCart.jsx
+++ src/Charts/BubbleCart.jsx
@@ -1,24 +1,57 @@
-import { Bubble } from 'react-chartjs-2';
-import ChartDataLabels from 'chartjs-plugin-datalabels';  // استيراد المكتبة
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ChartDataLabels);
+// Registering the ChartJS elements
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ArcElement, ChartDataLabels);
 
-const BubbleCart = ({ chartData }) => {
-    const options = {
-        plugins: {
-          datalabels: {
-            display: true,
-            formatter: (value) => value.name || '', 
-            color: 'black',
-            font: {
-              size: 14,
-            },
-          },
+const DoughnutCart = () => {
+  const data = {
+    labels: ['Philippines', 'India', 'Sri Lanka', 'Bangladesh', 'Indonesia'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: ["31", "22", "16", "16", "15"],
+        backgroundColor: ['#FF6384', '#FF9F40', '#FFCD56', '#4BC0C0', '#36A2EB'], // Colors
+        borderColor: ['#FF6384', '#FF9F40', '#FFCD56', '#4BC0C0', '#36A2EB'], // Border colors
+        borderWidth: 1,
+      }
+    ]
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false, // Disable maintaining aspect ratio
+    plugins: {
+      title: {
+        display: false, // إخفاء عنوان الرسم البياني إذا كنت لا تحتاجه
+      },
+      legend: {
+        display: false, // إخفاء أسطورة الرسم البياني
+      },
+      datalabels: {
+        anchor: 'center', // تحديد موضع الأرقام
+        align: 'center',
+        formatter: (value) => {
+          return `${value}%`; // تنسيق تسميات البيانات
         },
-      };
-  
-  return <Bubble data={chartData} options={options} />;
+      },
+    }
+  };
+  return (
+    <div className='flex md:flex-row justify-between flex-col items-center md:gap-0 gap-10 p-1 sm:h-[300px] h-[250px] '>
+      <div style={{height:"100%"}}>
+      <Doughnut data={data} options={options} />
+      </div>
+        <ul  className='gap-2 grid grid-cols-2 '>
+          <li className='flex items-center gap-2 justify-between'> <span className='bg-[#FF6384] w-[30px] h-[12px]'></span> <span>Philippines</span></li>
+          <li className='flex items-center gap-2 justify-between'> <span className='bg-[#FF9F40] w-[30px] h-[12px]'></span> <span>India</span></li>
+          <li className='flex items-center gap-2 justify-between'> <span className='bg-[#FFCD56] w-[30px] h-[12px]'></span> <span>Sri</span></li>
+          <li className='flex items-center gap-2 justify-between'> <span className='bg-[#4BC0C0] w-[30px] h-[12px]'></span> <span>Bangladesh</span></li>
+          <li className='flex items-center gap-2 justify-between'> <span className='bg-[#36A2EB] w-[30px] h-[12px]'></span> <span>Indonesia</span></li>
+        </ul>
+    </div>
+  );
 };
 
-export default BubbleCart;
+export default DoughnutCart;
--- src/Charts/LineCart.jsx
+++ src/Charts/LineCart.jsx
@@ -3,12 +3,25 @@ import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-const LineChart = ({ chartData }) => {
+const LineChart = () => {
   const options = {
     responsive: true,
   };
-
-  return <Line data={chartData} options={options} />;
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Countries graph workers',
+        data: [52, 59, 72, 60, 46, 55, 45],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+      },
+    ],
+  };
+  return<div >
+     <Line data={data} options={options} />
+  </div>;
 };
 
 export default LineChart;
